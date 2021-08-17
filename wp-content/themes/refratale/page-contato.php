854c41eba959d2d09e8e7105d2d305e653b28b28
Rei Medeiros <rm091291@gmail.com>
2021-08-17 20:56:26 -0300

    Finalização da página contato + correção da página sobre nós

--- wp-content/themes/refratale/page-contato.php
+++ wp-content/themes/refratale/page-contato.php
@@ -1,5 +1,67 @@
 <?php get_header(); ?>
 
-    <h1>Entrei nos CONTATOS</h1>
+<?php
+if (have_posts()) :
+    // query_posts(array('category_name'  => 'contato', 'posts_per_page' => 1));
+    while (have_posts()) : the_post();
+
+    
+        $image = get_field('imagem_parallax');
+        if (!empty($image)) :
+?>
+            <div class="parallax-container">
+                <div class="parallax">
+                    <img class="img-parallax" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                </div>
+            </div>
+            
+        <?php endif;  ?>
+        <div class="container margin-top40">
+            <div class="row">
+                <div class="col s12">
+                    <div class="row center-align">
+                        <h3>Contato</h3>
+                    </div>
+                    <div class="row">
+                        <?php 
+                           the_content();
+                        ?> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="fundo-cinza-claro">
+            <div class="container">
+                <div class="row center-align margin-bottom-20 padding-top20">
+                    
+                    <h3>
+                    <?php 
+                        if(get_field('area_cinza')){
+                            the_field('area_cinza');
+                        }
+                    ?> 
+                    </h3>
+                </div>
+                <div class="row btn-whatsapp center-align padding-bottom50">
+                    <a class="fonte-verde" href="<?php if(get_field('link_botao_whatsapp')){ the_field('link_botao_whatsapp');}?>" target="_blank">
+                    <?php 
+                        if(get_field('texto_botao_whatsapp')){
+                            the_field('texto_botao_whatsapp');
+                        }
+                    ?> 
+                    </a>
+                </div>
+            </div>
+        </div>
+
+<?php
+    
+    endwhile;
+else :
+    ?>
+    <p>Nenhum post cadastrado !</p>
+<?php
+endif;
+?>
 
 <?php get_footer(); ?>
--- wp-content/themes/refratale/page-sobre-nos.php
+++ wp-content/themes/refratale/page-sobre-nos.php
@@ -2,8 +2,9 @@
 <!-- Início IMG PARALLAX -->
 <?php
 if (have_posts()) :
-    query_posts(array('category_name'  => 'sobre_nos', 'posts_per_page' => 1));
+    // query_posts(array('category_name'  => 'sobre_nos', 'posts_per_page' => 1));
     while (have_posts()) : the_post();
+
         $image = get_field('imagem_parallax');
         if (!empty($image)) :
 ?>
@@ -12,96 +13,96 @@ if (have_posts()) :
                     <img class="img-parallax" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                 </div>
             </div>
-   <?php     endif;  ?>
-<div class="container margin-top40">
-    <div class="row">
-        <div class="col s12">
-            <div class="col s6">
-                <div class="row center-align">
-                    <h3>Sobre Nós</h3>
-                </div>
-                <div class="row descricao_sobre_nos">
-                    <p>
-                        <?php the_content();?>    
-                    </p>
-                </div>
-                <div class="row center-align">
-                    <h3>Missão</h3>
-                </div>
-                <div class="row descricao_sobre_nos">
-                    <p>
+        <?php endif;  ?>
+        <div class="container margin-top40">
+            <div class="row">
+                <div class="col s12">
+                    <div class="col s6">
+                        <div class="row center-align">
+                            <h3 class="margin-top0 margin-bottom0">Sobre Nós</h3>
+                        </div>
+                        <div class="row descricao_sobre_nos">
+                            <p>
+                                <?php the_content();?>    
+                            </p>
+                        </div>
+                        <div class="row center-align">
+                            <h3 class="margin-top0 margin-bottom0">Missão</h3>
+                        </div>
+                        <div class="row descricao_sobre_nos">
+                            <p>
 
-                    <?php 
-                        if(get_field('missao')){
-                            the_field('missao');
-                        }
-                    ?> 
-                    
-                    </p>
+                            <?php 
+                                if(get_field('missao')){
+                                    the_field('missao');
+                                }
+                            ?> 
+                            
+                            </p>
+                        </div>
+                        <div class="row center-align">
+                            <h3 class="margin-top0 margin-bottom0">Visão</h3>
+                        </div>
+                        <div class="row descricao_sobre_nos">
+                            <p>
+                            <?php 
+                                if(get_field('visao')){
+                                    the_field('visao');
+                                }
+                            ?> 
+                            </p>
+                        </div>
+                        <div class="row center-align">
+                            <h3 class="margin-top0 margin-bottom0">Valores</h3>
+                        </div>
+                        <div class="row descricao_sobre_nos">
+                            <p>
+                            <?php 
+                                if(get_field('valores')){
+                                    the_field('valores');
+                                }
+                            ?> 
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col s6 overflow-hidden">
+                        <?php
+                            $image = get_field('imagem_side');
+                            if (!empty($image)) :?>
+                                <img class="img-side" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                            <?php
+                            endif;
+                        ?>
+                    </div>
                 </div>
-                <div class="row center-align">
-                    <h3>Visão</h3>
-                </div>
-                <div class="row descricao_sobre_nos">
-                    <p>
-                    <?php 
-                        if(get_field('visao')){
-                            the_field('visao');
-                        }
-                    ?> 
-                    </p>
-                </div>
-                <div class="row center-align">
-                    <h3>Valores</h3>
-                </div>
-                <div class="row descricao_sobre_nos">
-                    <p>
-                    <?php 
-                        if(get_field('valores')){
-                            the_field('valores');
-                        }
-                    ?> 
-                    </p>
-                </div>
-            </div>
-            <div class="col s6 overflow-hidden">
-                <?php
-                    $image = get_field('imagem_side');
-                    if (!empty($image)) :?>
-                        <img class="img-side" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                    <?php
-                    endif;
-                ?>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="bg-cobertura">
-    <div class="container">
-        <div class="row center-align">
-                    <h3>Cobertura</h3>
-                </div>
-        <div class="row col s12">
-            <div class="col s6">
-                <?php
-                    $image = get_field('imagem_cobertura');
-                    if (!empty($image)) :?>
-                        <img class="img-cobertura" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                    <?php
-                    endif;
-                ?>
-            </div>
-            <div class="col s6 cidades">
-                <?php 
-                    if(get_field('cobertura')){
-                        the_field('cobertura');
-                    }
-                ?> 
             </div>
         </div>
-    </div>
-</div>
-<?php
+        <div class="bg-cobertura">
+            <div class="container">
+                <div class="row center-align padding-top10">
+                            <h3>Cobertura</h3>
+                        </div>
+                <div class="row col s12 padding-bottom20">
+                    <div class="col s6">
+                        <?php
+                            $image = get_field('imagem_cobertura');
+                            if (!empty($image)) :?>
+                                <img class="img-cobertura" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                            <?php
+                            endif;
+                        ?>
+                    </div>
+                    <div class="col s6 cidades">
+                        <?php 
+                            if(get_field('cobertura')){
+                                the_field('cobertura');
+                            }
+                        ?> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php
     
     endwhile;
 else :
@@ -110,4 +111,7 @@ else :
 <?php
 endif;
 ?>
+
+
+
 <?php get_footer(); ?>
